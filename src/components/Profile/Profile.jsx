391d504965140f83754561cdc61246e7015868bd
Sergiy Nikolaev <122089422+fsdev3@users.noreply.github.com>
2023-07-15 14:33:01 +0200

    upd

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,36 +1,29 @@
-import user from '../Json/user.json';
-import { Container } from './Profile.styled';
+import { Container, Info, Stats } from './Profile.styled';
 
-export const Profile = () => {
-  return user.map(data => {
-    return (
-      <Container key={data.id} className="profile">
-        <div className="description">
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-            alt="User avatar"
-            className="avatar"
-          />
-          <p className="name">Petra Marica</p>
-          <p className="tag">@pmarica</p>
-          <p className="location">Salvador, Brasil</p>
-        </div>
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <Container>
+      <Info>
+        <img src={avatar} alt={username} />
+        <p>{username}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </Info>
 
-        <ul className="stats">
-          <li>
-            <span className="label">Followers</span>
-            <span className="quantity">1000</span>
-          </li>
-          <li>
-            <span className="label">Views</span>
-            <span className="quantity">2000</span>
-          </li>
-          <li>
-            <span className="label">Likes</span>
-            <span className="quantity">3000</span>
-          </li>
-        </ul>
-      </Container>
-    );
-  });
+      <Stats>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </Stats>
+    </Container>
+  );
 };
--- src/components/Profile/Profile.styled.js
+++ src/components/Profile/Profile.styled.js
@@ -1,5 +1,21 @@
 import { styled } from 'styled-components';
 
 export const Container = styled.div`
-  background-color: blue;
+  margin: 60px auto;
+  background-color: gray;
+  width: 400px;
+  height: 500px;
+  padding: 100px;
+`;
+
+export const Info = styled.div`
+  background-color: red;
+  width: 400px;
+  /* padding: 10px 0 10px 40px; */
+`;
+
+export const Stats = styled.ul`
+  background-color: red;
+  width: 400px;
+  list-style: none;
 `;
