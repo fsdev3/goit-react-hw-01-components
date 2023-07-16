0fa63ab9c3517305219e9e66c0d4e228303207e7
Sergiy Nikolaev <122089422+fsdev3@users.noreply.github.com>
2023-07-16 20:40:19 +0200

    upd t4

--- src/components/Transactions/Transactions.styled.js
+++ src/components/Transactions/Transactions.styled.js
@@ -3,26 +3,49 @@ import { styled } from 'styled-components';
 export const Table = styled.table`
   margin: 60px auto 0 auto;
   width: 600px;
-
-  border: 1px solid gray;
-
-  tr {
-    border: 1px solid gray;
-    /* border-collapse: collapse; */
-  }
-  thead tr {
+  thead {
     background-color: #37c2ca;
     color: white;
+    text-transform: uppercase;
+    font-size: 13px;
+  }
+
+  th {
+    border: 1px solid #e8ebeb;
+  }
+
+  td {
+    color: gray;
+  }
+
+  td,
+  tr {
+    width: 33.33%;
+    height: 36px;
+    font-size: 13px;
+    font-weight: 500;
+    border: 1px solid #e8ebeb;
   }
 
   tbody tr:nth-child(even) {
-    background-color: #eaecec;
+    background-color: #f1f2f2;
   }
   tr td:first-child {
-    padding-left: 20px;
+    padding-left: 70px;
     text-transform: capitalize;
+  }
+  tr td:nth-child(2) {
+    text-align: right;
+    padding-right: 70px;
   }
   tr td:last-child {
     text-align: center;
   }
+
+  border-radius: 4px;
+  margin-bottom: 60px;
+  border-collapse: collapse;
+  box-shadow: 0px 3px 0px 0px rgba(209, 209, 209, 0.75);
+  -webkit-box-shadow: 0px 3px 0px 0px rgba(209, 209, 209, 0.75);
+  -moz-box-shadow: 0px 3px 0px 0px rgba(209, 209, 209, 0.75);
 `;
